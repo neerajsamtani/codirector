7a443561fff244c5058003117270e0f82a089c4f
Neeraj Samtani <neerajjsamtani@gmail.com>
2020-11-30 12:51:38 +0400

    Display Options and choose path at question node

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -3,6 +3,8 @@ import firebase from 'firebase/app'
 import firebaseConfig from '../firebase'
 import { database } from './Canvas'
 
+// TODO: Use this to play videos: https://github.com/CookPete/react-player
+
 const Watch = ({ projectId }) => {
 
     const [error, setError] = useState("")
@@ -10,9 +12,21 @@ const Watch = ({ projectId }) => {
     const [currentDisplay, setCurrentDiplay] = useState("")
 
     const nextGraphIndex = () => {
-        // setGraphIndex(graphIndex+1)
-        // console.log()
         const edge = graph.find(element => element.source && element.source === currentDisplay.id)
+        const nextNode = graph.find(element => element.id === edge.target)
+        setCurrentDiplay(nextNode)
+    }
+
+    const selectOption1 = () => {
+        const edge = graph.find(element => element.source && element.source === currentDisplay.id && 
+            ((element.sourceHandle === "answer1") || (element.sourceHandle === "option1")))
+        const nextNode = graph.find(element => element.id === edge.target)
+        setCurrentDiplay(nextNode)
+    }
+
+    const selectOption2 = () => {
+        const edge = graph.find(element => element.source && element.source === currentDisplay.id && 
+            ((element.sourceHandle === "answer2") || (element.sourceHandle === "option2")))
         const nextNode = graph.find(element => element.id === edge.target)
         setCurrentDiplay(nextNode)
     }
@@ -31,6 +45,7 @@ const Watch = ({ projectId }) => {
                     }
                     setGraph(initialGraph)
                     setCurrentDiplay(initialGraph[0]) // TODO: Create a START and END function
+                    console.log(initialGraph)
                 } else {
                     setError("Film not found")
                 }
@@ -39,12 +54,29 @@ const Watch = ({ projectId }) => {
     }
     useEffect(getGraph, [])
 
+    var controls = null
+    if (currentDisplay && currentDisplay.type === "questionNode") {
+        controls = (<div>
+            <p>{currentDisplay.data.question}</p>
+            <button onClick={selectOption1} >{currentDisplay.data.option1}</button>
+            <button onClick={selectOption2} >{currentDisplay.data.option2}</button>
+        </div>)
+    }
+    else if (currentDisplay && currentDisplay.type === "output") {
+        controls = <p>THE END</p>
+    }
+    else { // (currentDisplay.type === "videoNode" || currentDisplay.type === "input")
+        controls = (<div>
+            <button onClick={nextGraphIndex} >Next</button>
+        </div>)
+    }
+
     return (
         <div>
-            <p>Watch this movie</p>
+            {currentDisplay ? null : <p>Loading Film</p> }
             {error}
             {/* {console.log(graph)} */}
-            <button onClick={nextGraphIndex} >Next</button>
+            { controls }
             {currentDisplay ? <p>{JSON.stringify(currentDisplay)}</p> : null}
             {console.log(currentDisplay)}
         </div>
